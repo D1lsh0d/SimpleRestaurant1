8ef0a486cfdf018381d91296cb8e7f14591d5382
Dilshod <dilshod.akk2@gmail.com>
2024-08-20 12:22:31 +0500

    added Employee, ShickenOrder classes

---
+++ SimpleRestaurant1/Models/ChickenOrder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestaurant1.Models
+{
+    public class ChickenOrder
+    {
+        private int _quantity;
+        private bool _cooked;
+
+        public ChickenOrder(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Quantity must be more than 0");
+            }
+
+            _quantity = quantity;
+        }
+
+        public int GetQuantity() => _quantity;
+
+        public void CutUp()
+        {
+            for (int i = 0; i < _quantity; i++)
+            {
+                
+            }
+        }
+        public void Cook()
+        {
+            if (_cooked)
+            {
+                throw new Exception("The chiken was already cooked");
+            }
+
+            _cooked = true;
+        }
+        
+    }
+}
---
+++ SimpleRestaurant1/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRestaurant1.Models
+{
+    public static class Employee
+    {
+        public static void NewRequest()
+        {
+
+        }
+        public static void Inspect()
+        {
+
+
+        }
+        public static void CopyRequest()
+        {
+
+        }
+        public static void PrepareFood()
+        {
+
+        }
+
+    }
+}
